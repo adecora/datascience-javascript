13ed5449239222211cb2e3488a6c31f082af80c6
adecora <alejandrodecora@gmail.com>
2023-02-20 00:24:24 +0100

    15 - regular expressions

---
+++ regex/regex.js
@@ -0,0 +1,20 @@
+Tests = [
+  'Jamie: james@geneinfo.org',
+  'Zara: zetsure@bio123.edu',
+  'Hong and Andrzej: hchui@euphoric.edu and aszego@euphoric.edu'
+]
+
+const pattern = /[a-z]+@[a-z]+\.[a-z]+/g
+
+console.log(`pattern is ${pattern}`)
+for (let test of Tests) {
+  console.log(`tested against ${test}`)
+  const matches = test.match(pattern)
+  if (matches == null) {
+    console.log('-no matches-')
+  } else {
+    for (let m of matches) {
+      console.log(m)
+    }
+  }
+}
